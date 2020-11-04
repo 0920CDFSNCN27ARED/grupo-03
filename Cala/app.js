c852dddc5ded64eaeba25c74b3a81a7c3bcfd1b8
marurv <marinarvacatello@gmail.com>
2020-11-04 18:44:31 -0300

    comenzamos la estructura de los html

--- Cala/app.js
+++ Cala/app.js
@@ -1,2 +1,23 @@
 const express = require('express');
 const app = express();
+
+app.use(express.static('public'));
+
+app.listen(3000, ()=>{
+    console.log('Servidor funcionando');
+});
+
+app.all('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/index.html');
+});
+app.all('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.all('/products', (req,res)=>{
+    res.sendFile(__dirname + '/views/products.html');
+});
+
+app.all('/cart', (req,res)=>{
+    res.sendFile(__dirname + '/views/cart.html');
+});
