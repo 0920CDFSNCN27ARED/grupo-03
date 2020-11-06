d85e649b4c436cdb26a25c273c857c348656c189
Barbara-Fust <fbarbaram@gmail.com>
2020-11-06 13:40:20 -0300

    arreglo de rutas

--- Cala/src/app.js
+++ Cala/src/app.js
@@ -1,29 +1,28 @@
 const path = require("path");
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const pathALaCarpetaPublic = path.resolve(__dirname, "../public");
-const enrutadorDeExpressParaArchivosEstaticos = express.estatic(
+const enrutadorDeExpressParaArchivosEstaticos = express.static(
   pathALaCarpetaPublic
 );
 app.use(enrutadorDeExpressParaArchivosEstaticos);
 
-app.listen(3001, ()=>{
-    console.log('Servidor funcionando');
+app.listen(3001, () => {
+  console.log("Servidor funcionando");
 });
 
-app.all('/', (req,res)=>{
-    res.sendFile(path.resolve(__dirname, '../views/index.html'));
-
+app.all("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../views/index.html"));
 });
-app.all('/login', (req,res)=>{
-    res.sendFile(path.resolve(__dirname, '../views/login.html'));
-});
-
-app.all('/products', (req,res)=>{
-    res.sendFile(path.resolve(__dirname + '../views/products.html'));
+app.all("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../views/login.html"));
 });
 
-app.all('/cart', (req,res)=>{
-    res.sendFile(path.resolve(__dirname + '../views/cart.html'));
+app.all("/products", (req, res) => {
+  res.sendFile(path.resolve(__dirname + "../views/products.html"));
+});
+
+app.all("/cart", (req, res) => {
+  res.sendFile(path.resolve(__dirname + "../views/cart.html"));
 });
