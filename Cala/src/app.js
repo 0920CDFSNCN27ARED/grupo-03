08b147ccd714bc62f8e0f1cbd7f1a6f86c6a7735
Barbara-Fust <fbarbaram@gmail.com>
2020-11-05 22:25:52 -0300

    Camibie rutas a path

--- Cala/src/app.js
+++ Cala/src/app.js
@@ -1,23 +1,29 @@
+const path = require("path");
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'));
+const pathALaCarpetaPublic = path.resolve(__dirname, "../public");
+const enrutadorDeExpressParaArchivosEstaticos = express.estatic(
+  pathALaCarpetaPublic
+);
+app.use(enrutadorDeExpressParaArchivosEstaticos);
 
 app.listen(3001, ()=>{
     console.log('Servidor funcionando');
 });
 
 app.all('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(path.resolve(__dirname, '../views/index.html'));
+
 });
 app.all('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
+    res.sendFile(path.resolve(__dirname, '../views/login.html'));
 });
 
 app.all('/products', (req,res)=>{
-    res.sendFile(__dirname + '/views/products.html');
+    res.sendFile(path.resolve(__dirname + '../views/products.html'));
 });
 
 app.all('/cart', (req,res)=>{
-    res.sendFile(__dirname + '/views/cart.html');
+    res.sendFile(path.resolve(__dirname + '../views/cart.html'));
 });
