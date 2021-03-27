aa1710f2eaf759c4cf6ca82d131e553c15aa0f01
Barbara-Fust <fbarbaram@gmail.com>
2021-03-27 17:06:30 -0300

    Mejorando el boton de menu y sigo teniendo bugs

--- site/public/js/nav-bar-button.js
+++ site/public/js/nav-bar-button.js
@@ -1,11 +1,29 @@
 window.addEventListener("load", () => {
   const barButton = document.getElementById("bar-button");
   barButton.addEventListener("click", (event) => {
-    const navBar = document.getElementsById("nav-bar");
-    navBar.classList.toggle("hidden");
-    const ulNavBar = document.getElementById("ul-nav-bar");
+    const ulNavBar = document.querySelector("#ul-nav-bar");
+    ulNavBar.classList.remove("hidden");
+    ulNavBar.classList.add("show-sm");
     ulNavBar.classList.remove("header-menus");
     ulNavBar.classList.add("flex-column");
+    // const navBar = document.querySelector("#nav-bar");
+    // for(let i = 0; i < navBar.length; i ++){
+      
+    // }
     console.log("Está funcionando!! :)");
   });
+  
 });
+// window.addEventListener("load", () => {
+//   const barButton = document.getElementById("bar-button");
+//   barButton.addEventListener("click", (event) => {
+//     const ulNavBar = document.querySelector("#ul-nav-bar");
+//     ulNavBar.classList.add("hidden");
+//     ulNavBar.classList.remove("flex-column");
+//     ulNavBar.classList.add("header-menus");
+//     // const navBar = document.querySelector("#nav-bar");
+//     // for(let i = 0; i < navBar.clientHeight; i ++){
+//     //   navBar[i].classList.add("hidden");
+//     // }
+//      });
+// });
