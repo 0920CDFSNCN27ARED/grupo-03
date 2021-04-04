5f77d7f6a505abb5245d8370dc50aeb33f38b786
Barbara-Fust <fbarbaram@gmail.com>
2021-04-03 21:13:00 -0300

    Seguimos mejorando el front

--- site/public/js/nav-bar-button.js
+++ site/public/js/nav-bar-button.js
@@ -1,10 +1,13 @@
 window.addEventListener("load", () => {
   const barButton = document.getElementById("bar-button");
   barButton.addEventListener("click", (event) => {
-
+    const idNav = document.getElementById("id-nav");
+    idNav.classList.toggle("hidden");
     const ulNavBar = document.getElementById("ul-nav-bar");
     ulNavBar.classList.toggle("header-menus");
     ulNavBar.classList.toggle("flex-column");
+    ulNavBar.classList.toggle("z-index");
+
     const navBar = document.getElementsByClassName("nav-bar");
     for(let i = 0; i < navBar.length; i ++){
       navBar[i].classList.toggle("hidden");
--- site/public/js/search-button.js
+++ site/public/js/search-button.js
@@ -1,15 +1,18 @@
 window.addEventListener("load", () => {
   const searchButton = document.getElementById("search-button");
   searchButton.addEventListener("click", (event) => {
+    const searchInput = document.getElementById("search-input");
+      if(searchInput.value == ""){
+
     event.preventDefault();
     console.log("Está funcionando!! :)");
-    const searchInput = document.getElementById("search-input");
+    
     searchInput.classList.toggle("hidden");
     const searchDiv = document.getElementById("search-div")
     searchDiv.classList.toggle("no-border");
     searchDiv.classList.toggle("border-search");
 
-    
+    }
 
   });
 });
